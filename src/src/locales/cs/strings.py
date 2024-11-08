d77376c147c737aeaa88409254196b34dbae4edd
sodomak <cancel@cancel.cz>
2024-11-08 14:38:03 +0100

    Add localization system and string files

---
+++ src/src/locales/cs/strings.py
@@ -0,0 +1,11 @@
+STRINGS = {
+    # Window title
+    "app_title": "Virtuální Kamera s Pozadím",
+    
+    # Menu labels
+    "menu_file": "Soubor",
+    "menu_view": "Zobrazení",
+    "menu_help": "Nápověda",
+    
+    # ... rest of Czech strings ...
+}
---
+++ src/src/locales/en/strings.py
@@ -0,0 +1,11 @@
+STRINGS = {
+    # Window title
+    "app_title": "Virtual Camera Background",
+    
+    # Menu labels
+    "menu_file": "File",
+    "menu_view": "View",
+    "menu_help": "Help",
+    
+    # ... rest of English strings ...
+}
